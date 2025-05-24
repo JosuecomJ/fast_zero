975da82ab78d9ff70c2b3e5e96dc91a65beb4d31
Josué Nunes <josueemcontato@gmail.com>
2025-05-23 22:47:15 -0300

    adiciona endpoints para retornar mensagem em JSON e HTML; atualiza testes para validar novas rotas

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,38 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
-app = FastAPI()
+from fast_zero.schemas import Message
+
+app = FastAPI(
+    title='API Fast do Zero',
+    description='API Fast do Zero',
+    version='0.0.1',
+    docs_url='/docs',
+    redoc_url='/redoc',
+)
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá mundo!'}
+
+
+@app.get(
+    '/ola-mundo-html',
+    status_code=HTTPStatus.OK,
+    response_class=HTMLResponse,
+)
+def read_root_html():
+    return """
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>Olá Mundo</title>
+        </head>
+        <body>
+            <h1>Olá mundo!</h1>
+        </body>
+    </html>
+    """
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,4 @@
+import re
 from http import HTTPStatus
 
 from fastapi.testclient import TestClient
@@ -12,3 +13,14 @@ def test_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá mundo!'}
+
+
+def test_root_html_deve_retornar_ok_e_ola_mundo_html():
+    client = TestClient(app)
+
+    response = client.get('/ola-mundo-html')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.headers['content-type'] == 'text/html; charset=utf-8'
+    assert re.search(r'<h1>Olá mundo!</h1>', response.text)
+    assert re.search(r'<title>Olá Mundo</title>', response.text)
